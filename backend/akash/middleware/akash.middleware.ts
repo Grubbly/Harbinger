accf245b1da73fdb537742f3231adc8b06091a83
Grubbly <orthogonaltrajectory@gmail.com>
2021-07-12 18:51:39 -0800

    Create middleware file for akash

--- backend/akash/middleware/akash.middleware.ts
+++ backend/akash/middleware/akash.middleware.ts
@@ -6,6 +6,23 @@ const log: debug.IDebugger = debug('app:akash-middleware');
 
 class AkashMiddleware {
     async validateNoInvalidCharacters(
-        
-    )
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) {
+        const invalidCharacters: Array<String> = [
+            '&',';'
+        ];
+
+        // Do character check here
+        const reqCommandContainsInvalidCharacters: boolean = req.body.command;
+
+        if(reqCommandContainsInvalidCharacters) {
+            res.status(400).send({
+                error: `Invalid character found in command, commands cannot contain: ${invalidCharacters}`
+            });
+        } else {
+            next();
+        }
+    }
 }
