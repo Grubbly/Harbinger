6602e22206b57f3d21dd791f7ee3ef6016262dd7
Grubbly <orthogonaltrajectory@gmail.com>
2021-07-16 04:17:34 -0700

    If wallet is undefined in wallet profile, return to landing page

--- frontend/src/components/WalletProfile.vue
+++ frontend/src/components/WalletProfile.vue
@@ -1,13 +1,17 @@
 <template>
     <v-container class="d-flex justify-center" style="height: 90vh;">
         <v-row>
-            <v-col>
-                <v-card elevation='2'>
+            <v-col cols='12'>
+                <v-card class="fill-height" elevation='2'>
                     <v-card-title>{{this.walletName}}</v-card-title>
                     <v-card-text>{{this.wallet.address}}</v-card-text>
                 </v-card>
             </v-col>
+            <v-col cols='4' v-for="col in 9" :key="-col">
+                <v-card class="fill-height error">stuff</v-card>
+            </v-col>
         </v-row>
+
     </v-container>
 </template>
 
@@ -31,6 +35,11 @@ export default {
     methods: {
         getWalletByName(walletName) {
             const wallet = this.$store.getters.getWalletByName(walletName);
+
+            if(wallet === undefined) {
+                this.$router.push({ name: 'Home' });
+            }
+
             return wallet;
         }
     }
